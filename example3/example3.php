663459c8ad35d4ff06c096b47789d798f6729373
titanus <titanus@titanusx>
2017-07-21 08:44:10 -0500

    primeros cambios

---
+++ example3/example3.php
@@ -0,0 +1,17 @@
+<?php
+
+$mytest = 'this is a test';
+$mytest2 = 'this is a test of change 2';
+
+echo '<h1>'.$mytest.'</h1>';
+echo '<h1>'.$mytest2.'</h1>';
+echo '<h1>'.$mytest2.'</h1>';
+echo '<h1>'.$mytest2.'</h1>';
+
+echo 'this is new';
+echo '<h1>'.$mytest2.'</h1>';
+echo '<h1>'.$mytest2.'</h1>';
+echo '<h1>'.$mytest2.'</h1>';
+
+phpinfo();
+?>
